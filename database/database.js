ae984b872eeb91f96bd55f0fbb8d40189bf45beb
Connor Lopshire <lopshire.connor@gmail.com>
2022-04-20 14:56:24 -0500

    initial commit

---
+++ database/database.js
@@ -0,0 +1,52 @@
+const database = {
+    fish: [
+        {   species: "red fish",
+            name: "Jerry",
+            food: "shrimp",
+            size: 10,
+            harvested: "Australlia",
+            image: ""
+        },
+        {
+            species: "blue fish",
+            name: "Bart",
+            food: "crustaceans",
+            size: 3,
+            harvested: "Norway",
+            image: ""
+        },
+        {
+            species: "one fish",
+            name: "Larry",
+            food: "algae",
+            size: 2,
+            harvested: "China",
+            image: ""
+        },
+        {
+            species: "two fish",
+            name: "Dale",
+            food: "small fish",
+            size: 15,
+            harvested: "India",
+            image: ""
+        },
+        {
+            species: "mew fish",
+            name: "Derek",
+            food: "kelp",
+            size: 4,
+            harvested: "Fiji",
+            image: ""
+        },
+        {
+            species: "bew fish",
+            name: "Joel",
+            food: "crabs",
+            size: 8,
+            harvested: "Ireland",
+            image: ""
+        }
+    
+    ]
+}
---
+++ scripts/main.js
@@ -0,0 +1,6 @@
+// each fish should include the image, the species, its length, 
+// what you named the fish, where you harvested it, 
+// and what it eats
+const fishColection = {
+
+}
